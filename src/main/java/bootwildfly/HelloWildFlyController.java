caf037c9728265623894d73d6fab83c12bf0c34f
KevinChang <Kevin.Chang@trans-iot.com>
2017-10-03 15:00:13 +0800

    try other 3

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -3,6 +3,13 @@ package bootwildfly;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 @RestController
 public class HelloWildFlyController {
 
